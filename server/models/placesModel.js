a10aa0cf8c0786bf322517f91ebeafea9c97ad27
Scott Peterson <stpeterson28@yahoo.com>
2016-05-31 18:42:18 -0600

    Basic placeModel setup. Need to make the reviewSchema and photoSchema.

---
+++ server/models/placesModel.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const PlaceSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String
+    },
+    phoneNumber: {
+        type: Number
+    },
+    webite: {
+        type: String
+    },
+    openHours: {
+        monday: {
+            type: String
+        },
+        tuesday: {
+            type: String
+        },
+        wednesday: {
+            type: String
+        },
+        thursday: {
+            type: String
+        },
+        friday: {
+            type: String
+        },
+        saturday: {
+            type: String
+        },
+        sunday: {
+            type: String
+        }
+    },
+    rating: {
+        type: Number
+    },
+    reviews: [reviewSchema],
+    photos: [photoSchema],
+    googleId: {
+        type: String
+    },
+    placeType: {
+        type: String,
+    }
+})
+
+module.exports = mongoose.model('Place', PlaceSchema);
