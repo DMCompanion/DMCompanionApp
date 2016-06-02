4631fb18802a0e321f5e7bb327bd31b49ed2ace4
smcgee31 <solodesign31@gmail.com>
2016-06-02 00:43:31 -0600

    development of activitiesPage - images, styling

--- client/src/components/activities/activitiesSvc.js
+++ client/src/components/activities/activitiesSvc.js
@@ -4,39 +4,49 @@ angular.module('companion')
     const activities = [ {
         id: 0,
         name: 'Active Life',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Climbing', 'Biking', 'River Fun', ],
+        img: 'img/active.png'
     }, {
         id: 1,
+
         name: 'Arts & Entertainment',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too'],
+        img: 'img/artsy.png'
     }, {
         id: 2,
         name: 'Beauty & Spa',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Nails by Karam', 'Pedicures by Karam', 'Backrubs by Karam'],
+        img: 'img/spa.png'
     }, {
         id: 3,
         name: 'Food',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Food Trucks', 'Asian', 'Mexican', 'BBQ', 'Burgers'],
+        img: 'img/food.png'
     }, {
         id: 4,
         name: 'Night Life',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Nightclubs', 'Bars', 'Karam\'s Apartment'],
+        img: 'img/night.png'
     }, {
         id: 5,
         name: 'Outdoors',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Climmbing', 'Biking', 'Hiking', 'Water Sports'],
+        img: 'img/hiking.png'
     }, {
         id: 6,
         name: 'Religion',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too'],
+        img: 'img/church.png'
     }, {
         id: 7,
         name: 'Restaurants',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too'],
+        img: 'img/restaurants.png'
     }, {
         id: 8,
         name: 'Shopping',
-        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too']
+        items: ['Cool Stuff Here', 'And Here', 'Oh, and here too'],
+        img: 'img/shopping.png'
     }, ];
 
 
