a07688f7e43ed6bb9ed71f2cac50fb6e16d284a9
Ryan Hadley <ryanhadley@Ryans-MacBook-Pro.local>
2016-06-02 15:10:20 -0600

    adding crud to all pages

--- client/src/components/activities/activitiesCtrl.js
+++ client/src/components/activities/activitiesCtrl.js
@@ -15,4 +15,38 @@ angular.module( 'companion' )
 			return $scope.shownGroup === activity;
 		};
 
+
+		// CRUD ACTIVITIES
+			$scope.showActivities = () => {
+				calendarSvc.getActivities()
+				.then((response) => {
+					console.log(response);
+					// $scope.activities = response;
+				})
+			}
+
+			$scope.addActivity = (userActivity) => {
+				calendarSvc.createActivity(userActivity)
+				.then((response) => {
+					console.log(response);
+
+				})
+			}
+
+			$scope.updateActivity = (id, upActivity) => {
+				calendarSvc.editActivity(id, upActivity)
+				.then((response) => {
+					console.log(response);
+
+				})
+			}
+
+			$scope.destroyActivity = (id) => {
+				calendarSvc.deleteActivity(id)
+				.then((response) => {
+					console.log(response);
+
+				})
+			}
+
 	} );
--- client/src/components/places/placesSvc.js
+++ client/src/components/places/placesSvc.js
@@ -1,3 +1,40 @@
 angular.module('companion').service('placesSvc', ($http) => {
-  
+
+  // CRUD PLACES
+  this.getPlaces = () => {
+    return $http({
+      method: 'GET',
+      url: '/api/v1/places'
+    }).then((response) => {
+      return response;
+    })
+  }
+
+  this.createPlace = (place) => {
+    return $http({
+      method: 'POST',
+      url: '/api/v1/place',
+      data: activity
+    })
+  }
+
+  this.editPlace = (id, upPlace) => {
+    return $http({
+      method: 'PUT',
+      url: '/api/v1/activity/' + id,
+      data: upPlace
+    }).then((response) => {
+      return response;
+    })
+  }
+
+  this.deletePlace = (id) => {
+    return $http({
+      method: 'DELETE',
+      url: '/api/v1/place/' + id
+    }).then((response) => {
+      return response;
+    })
+  }
+
 });
