661322793dadae72c927e722691a66b1f287e0ce
semoog <sebmernst@gmail.com>
2016-06-06 15:29:08 -0600

    Beginnings of place merge with activities

--- client/src/components/places/placesCtrl.js
+++ client/src/components/places/placesCtrl.js
@@ -1,135 +1,24 @@
-angular.module('companion')
-  .controller('placesCtrl', ($scope, $http, $ionicModal) => {
+angular.module("companion").controller("placesCtrl", function($scope, placesSvc, $ionicGesture, $ionicHistory ) {
 
-    $scope.places = [
-      {
-        name: "CtPaTown",
-        placeType: "Chinese",
-        photos: ["https://res.cloudinary.com/teepublic/image/private/s--94mCrBS6--/t_Preview/b_rgb:42332c,c_limit,f_jpg,h_630,q_90,w_630/v1463160891/production/designs/511208_1.jpg"],
-        distance: 1.1,
-        rating: 2
-      },
-      {
-        name: "Sodosopa",
-        placeType: "American",
-        photos: ["https://res.cloudinary.com/teepublic/image/private/s--D51Ur500--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1446245008/production/designs/299463_1.jpg"],
-        distance: 1.8,
-        rating: 5
-      },
-      {
-        name: "City Wok",
-        placeType: "Chinese",
-        photos: ["http://www.citywok.com/images/logo.png"],
-        distance: 0.7,
-        rating: 3
-      },
-      {
-        name: "Wendell's Burgers",
-        placeType: "American",
-        photos: ["https://pbs.twimg.com/profile_images/649034487288987650/nufPHGv5.jpg"],
-        distance: 1.4,
-        rating: 4
-      },
-      {
-        name: "Wild Ginger",
-        placeType: "Japanese",
-        photos: ["https://res.cloudinary.com/teepublic/image/private/s--D51Ur500--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1446245008/production/designs/299463_1.jpg"],
-        distance: 1.3,
-        rating: 2
-      },
-      {
-        name: "Seven Eleven",
-        placeType: "Indian",
-        photos: ["http://www.citywok.com/images/logo.png"],
-        distance: 2.4,
-        rating: 2
-      }
-    ];
+  $scope.placesCategories = placesSvc.getDummyActivities();
 
-    $scope.tags = [{
-        name: 'Japanese',
-        selected: false
-      },
-      {
-        name: 'American',
-        selected: false
-      },
-      {
-        name: 'Chinese',
-        selected: false
-      },
-      {
-        name: 'Indian',
-        selected: false
-      },
-      {
-        name: 'Cantonese',
-        selected: false
-      },
-      {
-        name: 'Hawaiian',
-        selected: false
-      },
-    ];
+  $scope.goBack = function() {
+    $ionicHistory.goBack();
+  };
 
-    $scope.setSearchType = (index, type) => {
-      if ($scope.searchType !== type) {
-        for (var j = 0; j < $scope.tags.length; j++) {
-          $scope.tags[j].selected = false;
-        }
-        $scope.tags[index].selected = true;
-        $scope.searchType = type;
-      }
-      else {
-        $scope.tags[index].selected = false;
-        $scope.searchType = '';
-      }
+  $scope.toggleGroup = ( category ) => {
+    if ( $scope.isGroupShown( category ) ) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = category;
     }
+  };
+  $scope.isGroupShown = ( category ) => {
+    return $scope.shownGroup === category;
+  };
 
-    $ionicModal.fromTemplateUrl('templates/modal.html', {
-        scope: $scope
-      }).then(function(modal) {
-        $scope.modal = modal;
-      });
+  $scope.swipeRight = () => {
+      window.history.back();
+  };
 
-      $scope.createContact = function(u) {
-        $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
-        $scope.modal.hide();
-      };
-
-      // CRUD ACTIVITIES
-        $scope.showPlaces = () => {
-          calendarSvc.getPlaces()
-          .then((response) => {
-            console.log(response);
-            // $scope.places = response;
-          })
-        }
-
-        $scope.addPlace = (userPlace) => {
-          calendarSvc.createPlace(userPlace)
-          .then((response) => {
-            console.log(response);
-
-          })
-        }
-
-        $scope.updatePlace = (id, upPlace) => {
-          calendarSvc.editPlace(id, upPlace)
-          .then((response) => {
-            console.log(response);
-
-          })
-        }
-
-        $scope.destroyPlace = (id) => {
-          calendarSvc.deletePlace(id)
-          .then((response) => {
-            console.log(response);
-
-          })
-        }
-
-
-
-  });
+});
--- client/src/components/places/placesSvc.js
+++ client/src/components/places/placesSvc.js
@@ -1,4 +1,49 @@
-angular.module('companion').service('placesSvc', function($http) {
+angular.module('companion').service('placesSvc', function($http, $ionicHistory) {
+
+  this.getDummyActivities = () => {
+    return dummyActivities;
+  };
+
+  this.goBack = function() {
+    $ionicHistory.goBack();
+  };
+
+  const dummyActivities = [{
+    id: 0,
+    name: 'Restaurants',
+    items: [{
+      name: 'Yoga',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }, {
+      name: 'Biking',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }, {
+      name: 'Climbing',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }],
+    img: 'http://provofb.com/wp-content/uploads/2015/11/20160302-590-ProvoFoodBar3-1-640x425.jpg'
+  }, {
+    id: 1,
+
+    name: 'Shopping',
+    items: [{
+      name: 'Movies',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }, {
+      name: 'Concerts',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }, {
+      name: 'Museum',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      rating: `5-stars`
+    }],
+    img: 'http://reachprovo.com/wp-content/uploads/2016/04/Downtown-Provo-Utah-Center-Street.jpg'
+  }];
 
   // CRUD PLACES
   this.getPlaces = () => {
@@ -7,16 +52,16 @@ angular.module('companion').service('placesSvc', function($http) {
       url: '/api/v1/places'
     }).then((response) => {
       return response;
-    })
-  }
+    });
+  };
 
   this.createPlace = (place) => {
     return $http({
       method: 'POST',
       url: '/api/v1/place',
       data: activity
-    })
-  }
+    });
+  };
 
   this.editPlace = (id, upPlace) => {
     return $http({
@@ -25,8 +70,8 @@ angular.module('companion').service('placesSvc', function($http) {
       data: upPlace
     }).then((response) => {
       return response;
-    })
-  }
+    });
+  };
 
   this.deletePlace = (id) => {
     return $http({
@@ -34,10 +79,10 @@ angular.module('companion').service('placesSvc', function($http) {
       url: '/api/v1/place/' + id
     }).then((response) => {
       return response;
-    })
-  }
+    });
+  };
 
-  //CRUD reviews
+  // CRUD reviews
 
   this.getReviews = () => {
     return $http({
@@ -45,37 +90,37 @@ angular.module('companion').service('placesSvc', function($http) {
       url: '/api/v1/reviews'
     }).then((response) => {
       return response;
-    })
-  }
+    });
+  };
 
   this.getPlaceReviews = (id, reviewId) => {
     return $http({
       method: 'GET',
       url: '/api/v1/place/' + id + '/review/' + reviewId
-    })
-  }
+    });
+  };
 
   this.createReview = (id, review) => {
     return $http({
       method: 'POST',
       url: '/api/v1/place/' + id + '/review',
       data: review
-    })
-  }
+    });
+  };
 
   this.editReview = (id, upReview) => {
     return $http({
       method: 'PUT',
       url: '/api/v1/review/' + id,
       data: upReview
-    })
-  }
+    });
+  };
 
   this.deleteReview = (id, reviewId) => {
     return $http({
       method: 'DELETE',
       url: '/api/v1/place/' + id + '/review/' + reviewId
-    })
-  }
+    });
+  };
 
 });
