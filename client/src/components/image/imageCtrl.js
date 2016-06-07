ce2292386ef992f106c88f651de77e7c6714c870
Scott Peterson <stpeterson28@yahoo.com>
2016-06-07 11:01:53 -0600

    test 2

--- client/src/components/image/imageCtrl.js
+++ client/src/components/image/imageCtrl.js
@@ -1,19 +1,39 @@
 angular.module('companion')
   .controller('imageCtrl', function($scope, $cordovaCamera, $cordovaFile) {
 
-    $scope.getPicture = function(options) {
+    const Camera = $cordovaCamera;
 
-      var options = {
+    $scope.getPicture = (opts) => {
+
+      let options = {
         quality: 75,
         targetWidth: 400,
         targetHeight: 400,
         sourceType: 0
       };
 
-      Camera.getPicture(options).then(function(imageData) {
+      Camera.getPicture(options).then((imageData) => {
+        console.log(imageData);
         $scope.picture = imageData;
-      }, function(err) {
+      }, (err) => {
         console.log(err);
       });
+    };
+
+    $scope.takePicture = (opts) => {
+
+      let options = {
+        quality: 75,
+        targetWidth: 200,
+        targetHeight: 200,
+        sourceType: 1
+      };
+
+      Camera.getPicture(options).then((imageData) => {
+        $scope.picture = imageData;
+      }, (err) => {
+        console.log(err);
+      });
+
     };
   });
