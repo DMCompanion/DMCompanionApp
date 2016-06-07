7aa65192aaee2179ee9d42428658a01f7f910947
semoog <sebmernst@gmail.com>
2016-06-07 17:31:31 -0600

    Implemented places page distance to and time calculation

--- server/controllers/places.server.ctrl.js
+++ server/controllers/places.server.ctrl.js
@@ -1,6 +1,7 @@
 import Place from '../models/placesModel';
 import Review from '../models/reviewModel';
 import request from 'request';
+import config from '../../config'
 
 module.exports = {
 
@@ -41,13 +42,18 @@ module.exports = {
         });
     },
     getGooglePlaces: (req, res) => {
-      request
-        .get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + req.params.lat + ',' + req.params.long + '&radius=8000&types=' + req.params.type + '&hasNextPage=true&nextPage()=true&key=AIzaSyCL4CRH8G6V1OPP1oZT9LzOYWEB3ixMJjk&libraries=places')
-          .on('response', function(response) {
-            console.log(response);
-          })
-          .on('error', function(err) {
-            console.log(err);
-          });
+      request('https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + req.params.userLat + ',' + req.params.userLong + '&radius=8000&query=' + req.params.query + '&hasNextPage=true&nextPage()=true&key=' + config.googleMapsKey, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          res.send(body);
+        }
+      });
+    },
+    getGoogleDistance: (req, res) => {
+      request('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + req.params.userLat + ',' + req.params.userLong + '&destinations=place_id:' + req.params.placeId + '&key=' + config.googleMapsKey, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          res.send(body);
+        }
+      });
     }
+
 };
